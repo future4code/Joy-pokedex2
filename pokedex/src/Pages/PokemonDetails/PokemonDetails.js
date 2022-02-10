b2837c7de3236d0ac028798d95340eaf8cae78c8
Natalia Alves <nataliavalins@gmail.com>
2022-02-10 00:14:31 -0300

    estilizacao-da-pagina-de-detalhes

--- pokedex/src/Pages/PokemonDetails/PokemonDetails.js
+++ pokedex/src/Pages/PokemonDetails/PokemonDetails.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import axios from "axios";
 import { BASE_URL } from "../../Constants/BASE_URL";
-import { Container } from "./styled";
+import { Atacks, Container, ContainerInfo, PokemonBackView, PokemonFrontView, PokemonSize, PokemonStatus, PokemonType } from "./styled";
 
 const PokemonDetails = () => {
   const [pokemon, setPokemon] = useState(undefined);
@@ -34,17 +34,11 @@ const PokemonDetails = () => {
       <Header firstButton={"Pokedex"} secondButton={"Pokelist"} />
       
       {pokemon ? (
+        <div>
         <Container>
           <h1>{capitalizeFirstLetter(pokemon.name)}</h1>
-          <div>
-            <h3><strong>Principais ataques</strong></h3>
-            <p>{capitalizeFirstLetter(pokemon.moves[0].move.name)}</p>
-            <p>{capitalizeFirstLetter(pokemon.moves[1].move.name)}</p>
-            <p>{capitalizeFirstLetter(pokemon.moves[2].move.name)}</p>
-            <p>{capitalizeFirstLetter(pokemon.moves[3].move.name)}</p>
-          </div>
-          <div>
-            <h3><strong>Tipo</strong></h3>
+          <PokemonType>
+            <h4><strong>Tipo: </strong></h4>
             <p>
               {capitalizeFirstLetter(
                 pokemon &&
@@ -59,8 +53,17 @@ const PokemonDetails = () => {
                 pokemon.types[1].type &&
                 capitalizeFirstLetter(pokemon.types[1].type.name)}
             </p>
-          </div>
-          <div>
+          </PokemonType>
+          </Container>
+          <ContainerInfo>
+          <Atacks>
+            <h3><strong>Principais ataques</strong></h3>
+            <p>{capitalizeFirstLetter(pokemon.moves[0].move.name)}</p>
+            <p>{capitalizeFirstLetter(pokemon.moves[1].move.name)}</p>
+            <p>{capitalizeFirstLetter(pokemon.moves[2].move.name)}</p>
+            <p>{capitalizeFirstLetter(pokemon.moves[3].move.name)}</p>
+          </Atacks>
+          <PokemonSize>
             <h3><strong>Medidas</strong></h3>
             <p>
               {pokemon.height}00 <strong>cm</strong>
@@ -68,16 +71,17 @@ const PokemonDetails = () => {
             <p>
               {pokemon.weight}00 <strong>g</strong>
             </p>
-          </div>
-          <div>
+          </PokemonSize>
+         
+          <PokemonStatus>
             <h3><strong>Status</strong></h3>
             <p><strong>{capitalizeFirstLetter(pokemon.stats[0].stat.name)}</strong> = {pokemon.stats[0].base_stat}</p>
             <p><strong>{capitalizeFirstLetter(pokemon.stats[1].stat.name)}</strong> = {pokemon.stats[1].base_stat}</p>
             <p><strong>{capitalizeFirstLetter(pokemon.stats[2].stat.name)}</strong> = {pokemon.stats[2].base_stat}</p>
             <p><strong>{capitalizeFirstLetter(pokemon.stats[3].stat.name)}</strong> = {pokemon.stats[3].base_stat}</p>
             <p><strong>{capitalizeFirstLetter(pokemon.stats[4].stat.name)}</strong> = {pokemon.stats[4].base_stat}</p>
-          </div>
-          <div>
+          </PokemonStatus>
+          <PokemonFrontView>
             <img
               alt={pokemon.name}
               src={
@@ -89,8 +93,8 @@ const PokemonDetails = () => {
                 )
               }
             />
-          </div>
-          <div>
+          </PokemonFrontView>
+          <PokemonBackView>
             <img
               alt={pokemon.name}
               src={
@@ -102,8 +106,9 @@ const PokemonDetails = () => {
                 )
               }
             />
+          </PokemonBackView>
+          </ContainerInfo>
           </div>
-        </Container>
       ) : (
         <p>Carregando...</p>
       )}
